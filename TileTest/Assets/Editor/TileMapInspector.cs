3566a871658c4e0e897f19dc374d2986160af295
Jonathan-Velasquez <jfvgk4@mail.missouri.edu>
2015-02-13 13:43:02 -0600

    TileTest initial commit before data structure
    Jonathan Velasquez

---
+++ TileTest/Assets/Editor/TileMapInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(TileMap))]
+public class TileMapInspector : Editor {
+	public override void OnInspectorGUI(){
+		//base.OnInspectorGUI();
+		DrawDefaultInspector();
+		TileMap tileMap = (TileMap)target;
+		if (GUILayout.Button("Regenerate")) {
+			tileMap.BuildMesh();
+		}
+	}
+}
---
+++ TileTest/Assets/TileMapMouse.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TileMap))]
+public class TileMapMouse : MonoBehaviour {
+
+	TileMap _tileMap;
+
+	Vector3 currentTileCoord;
+
+	public Transform selectionCube;
+
+	void Start() {
+		_tileMap = GetComponent<TileMap> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hitInfo;
+
+		if (collider.Raycast (ray, out hitInfo, Mathf.Infinity)) {
+			int x = Mathf.FloorToInt(hitInfo.point.x/_tileMap.tileSize);
+			int z = Mathf.FloorToInt(hitInfo.point.z/_tileMap.tileSize);
+			Debug.Log("x:" + x + " z:" + z);
+			currentTileCoord.x = x;
+			currentTileCoord.z = z;
+
+			selectionCube.transform.position = currentTileCoord*3f;
+		}
+		else {
+		}
+		if(Input.GetMouseButtonDown (0)){
+			Debug.Log("CLick!");
+		}
+	}
+}
