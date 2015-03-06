70c0954c778110b4763079f31ca5410de0c5da4f
grace <rainspace.grace@gmail.com>
2015-03-06 12:57:44 -0600

    FogOfWar in progress

---
+++ FogOfWar/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInfo : MonoBehaviour
+{
+	public static Player player;
+	// Use this for initialization
+	void Start ()
+	{
+		createPlayer ();
+	}
+	void createPlayer(){
+		player = new Player (0, 0, 0);
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
+
