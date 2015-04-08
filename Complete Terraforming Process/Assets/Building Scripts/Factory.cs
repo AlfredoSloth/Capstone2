2e1aa716b07d38e687f2a39ad1e7887242b75029
grace <rainspace.grace@gmail.com>
2015-04-08 17:42:54 -0500

    Completed terraforming progress built on UI Picture Changes

---
+++ Complete Terraforming Process/Assets/Building Scripts/Factory.cs
@@ -0,0 +1,38 @@
+﻿public class Factory : Building {
+	//private 
+	private string pfcOrPhytoplankton;
+	private float releaseAmount;
+	
+	
+	//constructor
+	public Factory(string name, int cost, int turnsToBuild, int level,string pfcOrPhytoplankton, float releaseAmount) : base(name, cost, turnsToBuild, level){
+		setPfcOrPhytoplankton (pfcOrPhytoplankton);
+		setReleaseAmount (releaseAmount);
+	}
+	
+	private void setPfcOrPhytoplankton(string pfcOrPhytoplankton){
+		this.pfcOrPhytoplankton = pfcOrPhytoplankton;
+		
+	}
+	private void setReleaseAmount(float releaseAmount){
+		this.releaseAmount = releaseAmount;
+	}
+	
+	
+	public string getpfcOrPhytoplankton(){
+		return pfcOrPhytoplankton;
+	}
+	public float getReleaseAmount(){
+		return releaseAmount;
+	}
+	
+	public void updateReleaseAmount(float releaseAmount){
+		this.releaseAmount = releaseAmount;
+	}
+	
+	public override string buildingStats()
+	{
+		return "Name: " + this.getName () + "\nLevel: " + this.getLevel() + "\nPFC or Phytoplankton: " + this.getpfcOrPhytoplankton() + "\nRelease amount: " + this.getReleaseAmount();
+	}
+	
+}
---
+++ Complete Terraforming Process/Assets/Scripts/Mars.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mars{
+	private float averageTemp;
+	private bool marsHasMagnetosphere;
+	private float marsAtmosphere;
+	private float marsOxygen;
+	
+	public Mars (float temp, bool magnetosphere, float atmosphere, float oxygen){
+		setAverageTemp (temp);
+		setMarsHasMegnetosphere(magnetosphere);
+		setMarsAtmosphere(atmosphere);
+		setMarsOxygen(oxygen);
+	}
+	
+	public void setAverageTemp(float temp){
+		this.averageTemp = temp;
+	}
+	public void setMarsHasMegnetosphere(bool magnetosphere){
+		this.marsHasMagnetosphere = magnetosphere;
+	}
+	public void setMarsAtmosphere(float atmosphere){
+		this.marsAtmosphere = atmosphere;
+	}
+	public void setMarsOxygen(float oxygen){
+		this.marsOxygen = oxygen;
+	}
+	
+	public float getAverageTemp(){
+		return this.averageTemp;
+	}
+	public bool getMarsHasMegnetosphere(){
+		return this.marsHasMagnetosphere;
+	}
+	public float getMarsAtmosphere(){
+		return this.marsAtmosphere;
+	}
+	public float getMarsOxygen(){
+		return this.marsOxygen;
+	}
+	
+	public void updateAverageTemp(float temp){
+		this.averageTemp = temp;
+	}
+	public void updateMarsHasMegnetosphere(bool magnetosphere){
+		this.marsHasMagnetosphere = magnetosphere;
+	}
+	public void updateMarsAtmosphere(float atmosphere){
+		this.marsAtmosphere = atmosphere;
+	}
+	public void updateMarsOxygen(float oxygen){
+		this.marsOxygen = oxygen;
+	}
+}
