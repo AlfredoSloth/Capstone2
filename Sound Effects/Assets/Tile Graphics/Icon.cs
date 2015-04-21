2eb22c302272e82df29df08509b3dd9b163c6bde
LexusMans <lexus_mans@MacBookPro-C82A1425C4EC.local>
2015-04-21 00:39:09 -0500

    added sounds/manual/toggle buttons

---
+++ Sound Effects/Assets/Tile Graphics/Icon.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Icon : MonoBehaviour {
+	public List<ResourceIcon> icons;
+	public Icon(List<ResourceIcon> icons){
+		this.icons = icons;
+	}
+}
