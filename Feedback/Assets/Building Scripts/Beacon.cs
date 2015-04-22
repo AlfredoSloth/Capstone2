a398c8d251fa10e9d4507f7576acd382b140ba79
yiyi789 <yi_yi789@126.com>
2015-04-21 21:21:48 -0500

    t

--- Feedback/Assets/Building Scripts/Beacon.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Beacon : Building {
-
-	public Beacon(string name, int cost, int turnsToBuild, int level, int x, int y, int powered) : base(name, cost, turnsToBuild, level, x, y, powered){
-
-	}
-}
