f1c7c5bb14554010ebc7d74b4774c8a7645e296c
Jonathan-Velasquez <jfvgk4@mail.missouri.edu>
2015-04-23 06:47:25 -0500

    still working on the start location buildings. They keep being deleted from the player lists of buildings.

---
+++ Somewhat functional game/Assets/Building Scripts/ResearchLab.cs
@@ -0,0 +1,31 @@
+﻿public class ResearchLab : Building {
+	private int researchAmount;
+	public ResearchLab(string name, int cost, int turnsToBuild, int level, int x, int y, int powered, int researchPts) : base(name, cost, turnsToBuild, level, x, y, powered){
+		setResearchAmount(researchPts);
+	}
+	private void setResearchAmount(int researchAmount){
+		this.researchAmount = researchAmount;
+	}
+	
+	public int getResearchAmount(){
+		return researchAmount;
+	}
+	
+	public void updateResearchAmount(float releaseAmount){
+		this.researchAmount = researchAmount;
+	}
+	
+	public override string buildingStats()
+	{
+		string stat;
+		if (this.getPowered() == 0) {
+			stat = "false";
+		} else if(this.getPowered() == 1) {
+			stat = "true";
+		} else {
+			stat = "N/A";
+		}
+		
+		return "Name: " + this.getName () + "\nLevel: " + this.getLevel() + "\nPowered: " + stat + "\nResearch pts per turn: " + this.getResearchAmount();
+	}
+}
