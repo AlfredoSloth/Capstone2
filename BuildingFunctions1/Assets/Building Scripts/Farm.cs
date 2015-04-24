0a8dbc1380aacce62fb1bca640c954b124d18ccb
LexusMans <lexus_mans@mwc-126081.dhcp.missouri.edu>
2015-04-24 10:41:20 -0500

    Delete old folders

--- BuildingFunctions1/Assets/Building Scripts/Farm.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Farm : Building {
-	
-	public Farm(string name, int cost, int turnsToBuild, int level, int x, int y, int powered) : base(name, cost, turnsToBuild, level, x, y, powered){
-		
-	}
-	
-}
--- Modified construction+upgrade+turn controller/Assets/Building Scripts/Rover.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Rover : Building {
-	
-	public Rover(string name, int cost, int turnsToBuild, int level, int x, int y, int powered) : base(name, cost, turnsToBuild, level, x, y, powered){
-		
-	}
-}
