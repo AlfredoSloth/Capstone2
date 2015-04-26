0122e4d9bcbcbc3fef1b7049ba5cb277de1b6643
grace <rainspace.grace@gmail.com>
2015-04-26 05:27:43 -0500

    removed

--- BuildingFunctions+UI Style/Assets/Tile Graphics/Icon.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-public class Icon : MonoBehaviour {
-	public List<ResourceIcon> icons;
-	public Icon(List<ResourceIcon> icons){
-		this.icons = icons;
-	}
-}
