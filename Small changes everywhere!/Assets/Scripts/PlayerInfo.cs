fdfc563c917d1b943933044deff2e242de3eba48
Jonathan-Velasquez <jfvgk4@mail.missouri.edu>
2015-04-26 02:48:38 -0500

    small changes everywhere!

---
+++ Small changes everywhere!/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInfo : MonoBehaviour
+{
+	public GUIText Water;
+	public GUIText Food;
+	public GUIText Building;
+	public static Player player;
+	public int water=0;
+	public int food=0;
+	public int buildingResource=0;
+	public static Mars mars;
+	public static GameObject[,] buildings;
+	public static Rover firstRover;
+	public static Farm firstFarm;
+	public static Beacon firstBeacon;
+	public static Base firstBase;
+	public static Powerplant firstPowerPlant;
+	public static Warehouse firstWarehouse;
+	public static int placeRoverX;
+	public static int placeRoverY;
+	public static GameObject rover;
+	
+	TGMap _tileMapGfx;
+	// Use this for initialization
+	void Start ()
+	{
+		_tileMapGfx = GetComponent<TGMap> ();
+		createPlayer ();
+		mars = new Mars (-80, false, 5.00f, 0.13f);
+		buildings = new GameObject[TGMap.size_x,TGMap.size_z];
+	}
+	void createPlayer(){
+		player = new Player (100, 100, 100, 100);
+	}
+	//This function is going to be called every turn to get the data updated.
+	//still need to 
+	public void addScore(){
+		water = player.getWater();
+		food = player.getFood();
+		buildingResource = player.getBuildingResources();
+		UpdateScore ();
+	}
+	// Update is called once per frame
+	void UpdateScore ()
+	{
+		Water.text = water.ToString();
+		Food.text = food.ToString();
+		Building.text = buildingResource.ToString();
+	}
+	public static void createStartLocation(){
+		placeRoverX = Random.Range (50, 40);
+		placeRoverY = Random.Range (20, 30);
+		
+		//placing first rover in data
+		firstRover = new Rover ("Rover", placeRoverX, placeRoverY, 2, 4);
+		TGMap.map.GetTileAt (placeRoverX, placeRoverY).updateHasRover (firstRover);
+		
+		//placing Farm in data
+		int farmCost = PlayerInfo.player.getCostAtLevel1("Farm");
+		firstFarm = new Farm ("Farm", farmCost, 0, 1, placeRoverX + 2, placeRoverY, 0, 25);
+		TGMap.map.GetTileAt(placeRoverX+2, placeRoverY).updateBulding(firstFarm);
+		firstFarm.updateCost (firstFarm.getCost() * 2);
+		
+		//placing Beacon in data
+		int beaconCost = PlayerInfo.player.getCostAtLevel1("Beacon");
+		firstBeacon = new Beacon ("Beacon", beaconCost, 0, 1, placeRoverX - 1, placeRoverY, 1);
+		TGMap.map.GetTileAt(placeRoverX-1, placeRoverY).updateBulding(firstBeacon);
+		firstBeacon.updateCost (firstBeacon.getCost() * 2);
+		//PlayerInfo.player.insertBeacon (firstBeacon);
+		
+		//placing warehouse in data
+		int warehouseCost = PlayerInfo.player.getCostAtLevel1("Warehouse");
+		firstWarehouse = new Warehouse ("Warehouse", warehouseCost, 0, 1, placeRoverX - 1, placeRoverY - 1, 0);
+		TGMap.map.GetTileAt(placeRoverX-1, placeRoverY-1).updateBulding(firstWarehouse);
+		firstWarehouse.updateCost (firstWarehouse.getCost () * 2);
+		
+		//placing powerplant in data
+		int powerPlantCost = PlayerInfo.player.getCostAtLevel1("Power Plant");
+		firstPowerPlant = new Powerplant ("Power Plant", powerPlantCost, 0, 1, placeRoverX - 1, placeRoverY + 1, 1, 0);
+		TGMap.map.GetTileAt(placeRoverX-1, placeRoverY+1).updateBulding(firstPowerPlant);
+		firstPowerPlant.updateCost (firstPowerPlant.getCost () * 2);
+		
+		//placing base in data
+		int baseCost = PlayerInfo.player.getCostAtLevel1("Base");
+		firstBase = new Base ("Base", baseCost, 0, 1, placeRoverX, placeRoverY + 1, 0, 4);
+		TGMap.map.GetTileAt(placeRoverX, placeRoverY+1).updateBulding(firstBase);
+		firstBase.updateCost (firstBase.getCost () * 2);
+		//take off the cost to build level 1
+		//update the cost to upgrade to level2
+		
+		
+		Vector3 location1 = TGMap.elevationMap [placeRoverX, placeRoverY].transform.position;
+		Vector3 location2 = TGMap.elevationMap [placeRoverX + 2, placeRoverY].transform.position;
+		Vector3 location3 = TGMap.elevationMap [placeRoverX - 1, placeRoverY].transform.position;
+		Vector3 location4 = TGMap.elevationMap [placeRoverX - 1, placeRoverY - 1].transform.position;
+		Vector3 location5 = TGMap.elevationMap [placeRoverX - 1, placeRoverY + 1].transform.position;
+		Vector3 location6 = TGMap.elevationMap [placeRoverX, placeRoverY + 1].transform.position;
+		location4.y += .25f;
+		location3.y += .23f;
+		GameObject rover = Instantiate (Resources.Load("roverPrefab"), location1, Quaternion.identity) as GameObject;
+		TurnController.rovers[placeRoverX,placeRoverY]=rover;
+		GameObject farm = Instantiate (Resources.Load("FarmPrefab"), location2, Quaternion.identity) as GameObject;
+		GameObject beacon = Instantiate (Resources.Load("BeaconPrefab"), location3, Quaternion.identity) as GameObject;
+		GameObject warehouse = Instantiate (Resources.Load("WarehousePrefab"), location4, Quaternion.identity) as GameObject;
+		GameObject powerPlant = Instantiate (Resources.Load("PowerPlantPrefab"), location5, Quaternion.identity) as GameObject;
+		GameObject playerBase = Instantiate (Resources.Load("BasePrefab"), location6, Quaternion.identity) as GameObject;
+		location6.y += 13;
+		location6.z -= 1;
+		Camera.main.transform.position = location6;
+		FogMap functions = new FogMap ();
+		functions.checkFog ();
+	}
+	public static void insertStarterBuildings(Base firstBase, Farm firstFarm, Beacon firstBeacon, Powerplant firstPowerPlant, Warehouse firstWarehouse, Rover firstRover, GameObject rover){
+		firstBeacon.createSafeZones ();
+		firstPowerPlant.powerUp ();
+		firstBase.updateSupplier ();
+		firstWarehouse.supplyBase ();
+		PlayerInfo.player.insertBase (firstBase);
+		PlayerInfo.player.insertFarm (firstFarm);
+		PlayerInfo.player.insertBeacon (firstBeacon);
+		PlayerInfo.player.insertPowerplant (firstPowerPlant);
+		PlayerInfo.player.insertWarehouse (firstWarehouse);
+		TGMap.map.GetTileAt (firstRover.getPositionX(), firstRover.getPositionY()).updateHasRover (firstRover);
+		TurnController.rovers[placeRoverX,placeRoverY]=rover;
+	}
+}
---
+++ Small changes everywhere!/Assets/Tile Data/TDTile.cs
@@ -0,0 +1,169 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TDTile{
+	
+	public const int TILE_MOUNTAIN = 0;
+	public const int TILE_FLAT = 1;
+	public const int TILE_HILLS = 2;
+	public const int TILE_VALLEY = 3;
+	
+	private int terraformed;
+	private int terrainType;
+	private List<TileResource> resources;
+	private Building hasBuilding;
+	private Rover hasRover;
+	private bool isSafe;
+	private bool hasIce;
+	private bool isVisable;
+	private Vector2 baseVector;
+	public TDTile(int lowerOrHigher){
+		setTerrainType (lowerOrHigher);
+
+		
+		setResources();
+		setHasBuilding ();
+		setTerriformed (/*terriform*/);
+		setIsSafe ();
+		setHasIce();
+		hasRover = null;
+		isVisable = false;
+		baseVector = new Vector2(-1f,-1f);
+	}
+	private void setTerrainType(int lowerOrHigher){
+		int type = Random.Range (1,100);
+		if (lowerOrHigher == 0) {
+			if (type >= 1 && type <= 40) {
+				this.terrainType=1;
+			} else if (type > 40 && type <= 80) {
+				this.terrainType = 2;
+			}else if (type > 80 && type<= 90) {
+				this.terrainType = 3;
+			}
+			else if (type > 90 && type <= 100) {
+				this.terrainType = 0;
+			}
+		} else {
+			if (type >= 1 && type <= 80) {
+				this.terrainType=1;
+			} else if (type > 85 && type <= 87) {
+				this.terrainType=3;
+			} else if (type > 87 && type <= 95) {
+				this.terrainType=1;
+			} else if (type > 95 && type <= 100) {
+				this.terrainType=2;
+			}
+		}
+	}
+	private void setResources(){
+		resources = new List<TileResource> ();
+		var resourceType = Random.Range (1,1000);
+		if (resourceType > 1 && resourceType < 750) {
+			TileResource none = new TileResource("none", 0);
+			resources.Add(none);
+		} else if (resourceType >= 750 && resourceType < 900) {
+			TileResource building = new TileResource("building", Random.Range(10,20));
+			resources.Add(building);
+			int secondResource = Random.Range (1,10);
+			if(secondResource>=1&&secondResource<=5){
+				TileResource water = new TileResource("water", Random.Range(5,10));
+				resources.Add(water);
+			}
+		} else if (resourceType >= 900 && resourceType <= 1000) {
+			TileResource water = new TileResource("water", Random.Range(5,10));
+			resources.Add(water);
+			int secondResource = Random.Range (1,10);
+			if(secondResource>=1&&secondResource<=5){
+				TileResource building = new TileResource("building", Random.Range(10,20));
+				resources.Add(building);
+			}
+		}
+	}
+	private void setHasBuilding(){
+		hasBuilding = null;
+	}
+	private void setTerriformed(/*int terriform*/){
+		//this.terraformed = terriform
+	}
+	private void setIsSafe(){
+		isSafe = false;
+	}
+	private void setHasIce(){
+		hasIce = false;
+	}
+	public int getTerrainType(){
+		return terrainType;
+	}
+	public List<TileResource> getResources(){
+		return resources;
+	}
+	public int getTerriform(){
+		return terraformed;
+	}
+	public Building getBuilding(){
+		return hasBuilding;
+	}
+	public bool getIsSafe(){
+		return isSafe;
+	}
+	public bool getHasIce(){
+		return hasIce;
+	}
+	public Rover getHasRover(){
+		return hasRover;
+	}
+	public bool getIsVisable(){
+		return isVisable;
+	}
+	public Vector2 getIsGathered(){
+		return baseVector;
+	}
+	public void updateIsSafe(bool changedValue){
+		this.isSafe = changedValue;
+	}
+	public void updateHasIce(bool changedValue){
+		this.hasIce = changedValue;
+	}
+	public void updateIsGathered(Vector2 baseVector){
+		this.baseVector = baseVector;
+	}
+	public void updateBulding(Building newBuilding){
+		this.hasBuilding = newBuilding;
+	}
+	public void updateTerriform(int newTerriform){
+		
+		this.terraformed = newTerriform;
+	}
+	public void updateHasRover(Rover newValue){
+		this.hasRover = newValue;
+	}
+	public void updateIsVisable(bool newValue){
+		this.isVisable = newValue;
+	}
+	
+	public void updateResourceAmount(string resource, int newValue){
+		List<TileResource> check = getResources (); 
+		foreach (TileResource tileResource in check){
+			if(tileResource.resource.Equals(resource)){
+				tileResource.resourceAmount = newValue;
+			}
+		}
+		this.resources = check;
+	}
+	public void addResouce(string resource, int newValue){
+		TileResource newResource = new TileResource (resource, newValue);
+		resources.Add(newResource);
+	}
+	public void removeResource(string resource){
+		List<TileResource> check = getResources (); 
+		TileResource remove;
+		foreach (TileResource tileResource in check){
+			if(tileResource.resource.Equals(resource)){
+				remove = tileResource;
+				this.resources.Remove(remove);
+			}
+		}
+	}
+	
+}
